24688c399cb599cfc0fcc9039181e1d00e0a865b
Nikita <none.in.nowhere@gmail.com>
2025-09-19 23:20:49 +0300

    1st task 1-5 init commit

---
+++ uni_307/task1/solution_template (22)/task15.py
@@ -0,0 +1,23 @@
+from typing import List
+
+
+def hello(name: str) -> str:
+    pass
+
+
+def int_to_roman(num: int) -> str:
+    pass
+
+
+def longest_common_prefix(strs_input: List[str]) -> str:
+    pass
+
+
+def primes() -> int:
+    yield
+
+
+class BankCard:
+    def __init__(self, total_sum: int, balance_limit: int):
+        pass
+
